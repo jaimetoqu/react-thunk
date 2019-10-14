717c68274073b183ff840178536fbc2c054011d7
eduardo <eduardo@MacBook-Pro-de-NB661.local>
2019-10-14 15:41:10 -0300

    app asynchronous with react-thunk

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import index from "./js/index"
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// serviceWorker.unregister();
